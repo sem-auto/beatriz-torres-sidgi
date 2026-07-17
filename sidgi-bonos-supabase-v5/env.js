94cac91997cd36483669caa4a5e0b79284858773
sem-auto <semserviciosgenerales1@gmail.com>
2026-07-17 11:46:00 +0200

    Delete sidgi-bonos-supabase-v5 directory

--- sidgi-bonos-supabase-v5/env.js
+++
@@ -1,7 +0,0 @@
-window.SIDGI_ENV = {
-  SUPABASE_URL: 'https://voawzmrluhlqebccivgf.supabase.co',
-
-  SUPABASE_ANON_KEY: 'sb_publishable_9Op-8bUfYeT7hyCx7bmWEQ_04Z_z9sf',
-
-  ORGANIZATION_ID: '2daaa83a-e78d-4b99-96f0-8c141facb2a9'
-};
